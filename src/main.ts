eda9cdd1b22b48c7bd980677f8cf1a71ada27b41
Kevin Escoto <krodriguez@applaudostudios.com>
2021-03-13 11:47:26 -0600

    ✨ chore: update nestjs bootstrap config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
+  const apiPrefix = configService.get('API_PREFIX') || 'api/v1';
+  app.setGlobalPrefix(apiPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, validationError: { target: false } }));
+
+  const options = new DocumentBuilder()
+    .setTitle('NodeJS Crew C1')
+    .setDescription('NodeJS Crew Challenge 1')
+    .setVersion('1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`${apiPrefix}/docs`, app, document);
+
+  await app.listen(port);
 }
 bootstrap();
